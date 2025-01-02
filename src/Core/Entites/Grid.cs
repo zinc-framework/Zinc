48d40cd7ac0747528f3946f0b315d99718859a57
Kyle Kukshtel <kyle.kukshtel@gmail.com>
2025-01-02 00:51:29 -0500

    better grid param names

--- src/Core/Entites/Grid.cs
+++ src/Core/Entites/Grid.cs
@@ -7,14 +7,14 @@ namespace Zinc;
 public partial class Grid : SceneEntity
 {
     private readonly Action<Entity, double>? _updateWrapper;
-    public Grid(int cellWidth = 8, int cellHeight = 8, int horizontalDimension = 4, int verticalDimension = 4, Scene? scene = null, bool startEnabled = true, Action<Grid,double>? update = null, Anchor? parent = null, List<Anchor>? children = null)
+    public Grid(int cellWidth = 8, int cellHeight = 8, int numHorizontalCells = 4, int numVerticalCells = 4, Scene? scene = null, bool startEnabled = true, Action<Grid,double>? update = null, Anchor? parent = null, List<Anchor>? children = null)
         : base(startEnabled,scene,parent:parent,children:children)
     {
         Name = "Grid";
         CellWidth = cellWidth;
         CellHeight = cellHeight;
-        NumHorizonalCells = horizontalDimension;
-        NumVerticalCells = verticalDimension;
+        NumHorizonalCells = numHorizontalCells;
+        NumVerticalCells = numVerticalCells;
         GridPivot = new Vector2(0.5f);
         CellPivot = new Vector2(0.5f);
         if (update != null && _updateWrapper == null)
