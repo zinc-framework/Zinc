95894404ea215b3b6c4713c06442712245ec0f40
Kyle Kukshtel <kyle.kukshtel@gmail.com>
2025-01-05 23:47:13 -0500

    change default anchor position to 0,0

--- src/Core/Entites/Entity.cs
+++ src/Core/Entites/Entity.cs
@@ -318,8 +318,8 @@ public partial class SceneEntity : Anchor
     public SceneEntity(bool startEnabled, Scene? scene = null, Action<Entity, double>? update = null, Anchor? parent = null, List<Anchor>? children = null) 
         : base(startEnabled, scene, parent,children)
     {
-        X = Engine.Width/2f;
-        Y = Engine.Height/2f;
+        X = 0;
+        Y = 0;
         Update = update;
     }
 }
